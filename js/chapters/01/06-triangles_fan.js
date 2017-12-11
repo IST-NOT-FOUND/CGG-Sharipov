60c4dddeec3e129de9d9a08999c7d1886eb92380
qusijue <qusijue@gmail.com>
2017-12-12 01:39:25 +0500

    chore (js\chapters\01\06-triangles_fan.js) add function draw

--- js/chapters/01/06-triangles_fan.js
+++ js/chapters/01/06-triangles_fan.js
@@ -42,3 +42,12 @@ function initBuffers() {
   vertexBuffer.itemSize = 3;
   vertexBuffer.numberOfItems=5;
 }
+
+function draw() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+                         vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertexBuffer.numberOfItems);
+}
