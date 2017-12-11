add67b0b7fbd21903be206696c21fd513a024386
qusijue <qusijue@gmail.com>
2017-12-12 02:51:57 +0500

    chore (js\chapters\03\01-color_setting.js) add function getShader

--- js/chapters/03/01-color_setting.js
+++ js/chapters/03/01-color_setting.js
@@ -19,3 +19,16 @@ function initShaders() {
     shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
     gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 }
+
+function getShader(type, id) {
+    var source = document.getElementById(id).innerHTML;
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Shader compilation error: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
