4b0c84eb5d55dd03a0999aaa12f84a95bda22cc6
qusijue <qusijue@gmail.com>
2017-12-12 01:12:55 +0500

    chore (js/chapters/01/01-context.js) init function

---
+++ js/chapters/01/01-context.js
@@ -0,0 +1,14 @@
+var gl = null; 
+window.onload = function () {
+    var canvas = document.getElementById("canvas3D");
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    } catch (e) {}
+    if (!gl) {
+        alert("Your brouser doesn't support WebGL");
+    }
+    if (gl) {
+        gl.clearColor(0.5, 0.5, 0.5, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+};
