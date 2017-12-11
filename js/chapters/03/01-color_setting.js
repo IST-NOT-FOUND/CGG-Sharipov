191bd343215505fabae0ae58cdd2e838ec253b99
qusijue <qusijue@gmail.com>
2017-12-12 02:52:48 +0500

    chore (js\chapters\03\01-color_setting.js) add function draw

--- js/chapters/03/01-color_setting.js
+++ js/chapters/03/01-color_setting.js
@@ -53,3 +53,16 @@ function initBuffers() {
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(сolors), gl.STATIC_DRAW);
 }
+
+function draw() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+            vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+            vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.numberOfItems);
+}
