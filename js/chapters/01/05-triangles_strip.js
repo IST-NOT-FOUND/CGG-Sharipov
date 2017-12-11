d1d6a016aa2d09cdacd85b4ebe5c9cdce081c760
qusijue <qusijue@gmail.com>
2017-12-12 01:27:18 +0500

    chore (js\chapters\01\05-triangles_strip.js) add function initShaders

---
+++ js/chapters/01/05-triangles_strip.js
@@ -0,0 +1,18 @@
+var gl;
+var shaderProgram;
+var vertexBuffer;
+
+function initShaders() {
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Failed to install shaders");
+    }
+    gl.useProgram(shaderProgram);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+}
