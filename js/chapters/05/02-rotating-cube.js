f13e1a09a46bc0081ee996039f6526d3a5fd7ac7
qusijue <qusijue@gmail.com>
2017-12-12 04:15:46 +0500

    chore (js\chapters\05\02-rotating-cube.js) add function setupWebGL

--- js/chapters/05/02-rotating-cube.js
+++ js/chapters/05/02-rotating-cube.js
@@ -72,3 +72,14 @@ function draw() {
 
     gl.drawElements(gl.LINES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
 }
+
+function setupWebGL() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    angle += 0.01;
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    mat4.perspective(pMatrix, 1.04, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
+    mat4.identity(mvMatrix);
+    mat4.translate(mvMatrix,mvMatrix,[0, 0, -2.0]);
+    mat4.rotate(mvMatrix,mvMatrix, angle, [0, 1, 0]);
+}
