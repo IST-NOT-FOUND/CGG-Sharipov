808134e92dedbf4f533a2d17181f7a7c2929ee5b
qusijue <qusijue@gmail.com>
2017-12-12 04:11:23 +0500

    chore (js\chapters\05\01-moving-cube.js) add function setupWebGL

--- js/chapters/05/01-moving-cube.js
+++ js/chapters/05/01-moving-cube.js
@@ -72,3 +72,13 @@ function draw() {
         vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
     gl.drawElements(gl.LINES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
 }
+
+function setupWebGL() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    mat4.perspective(pMatrix, 1.04, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
+    mat4.identity(mvMatrix);
+    mat4.translate(mvMatrix,mvMatrix,[0, 0, zTranslation]);
+    mat4.rotate(mvMatrix,mvMatrix, angle, [0, 1, 0]);
+}
