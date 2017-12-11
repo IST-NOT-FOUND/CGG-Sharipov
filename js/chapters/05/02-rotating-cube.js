7991c50963e295d51fc28c35e24e953951db3a99
qusijue <qusijue@gmail.com>
2017-12-12 04:13:54 +0500

    chore (js\chapters\05\02-rotating-cube.js) add function initShaders

---
+++ js/chapters/05/02-rotating-cube.js
@@ -0,0 +1,24 @@
+var gl;
+var shaderProgram;
+var vertexBuffer;
+var indexBuffer;
+var angle = 0.0;
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+function initShaders() {
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Failed to install shaders");
+    }
+    gl.useProgram(shaderProgram);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.MVMatrix = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.ProjMatrix = gl.getUniformLocation(shaderProgram, "uPMatrix");
+}
