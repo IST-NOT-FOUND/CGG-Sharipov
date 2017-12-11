88dd90aad3fad676c678ecedceaa76cddc0f68b5
qusijue <qusijue@gmail.com>
2017-12-12 01:43:03 +0500

    chore (js\chapters\01\08-homework_1.js) add function init

---
+++ js/chapters/01/08-homework_1.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    init();
+};
+
+function init(){
+    var canvas = document.getElementById('canvas3D');
+    var gl = canvas.getContext('experimental-webgl');
+    var vertices = [
+        -0.5, 0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.5, 0.5, 0.0
+    ];
+    indices = [3, 2, 1, 3, 1, 0];
+    var vertex_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    var Index_Buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    var vertCode =
+            'attribute vec3 coordinates;' +
+            'void main(void) {' +
+            ' gl_Position = vec4(coordinates, 1.0);' +
+            '}';
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertCode);
+    gl.compileShader(vertShader);
+    var fragCode =
+            'void main(void) {' +
+            ' gl_FragColor = vec4(0.09, 0.12, 0.15, 1.0);' +
+            '}';
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, fragCode);
+    gl.compileShader(fragShader);
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertShader);
+    gl.attachShader(shaderProgram, fragShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);
+    gl.clearColor(getRandomValue(0, 100), getRandomValue(0, 100), getRandomValue(0, 100), 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+};
