ee39f177217ca7b26debb7d63c061ad69676f167
qusijue <qusijue@gmail.com>
2017-12-12 04:17:47 +0500

    chore (js\chapters\05\03-homework_1.js) add function init

---
+++ js/chapters/05/03-homework_1.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    init();
+};
+var camera, scene, renderer;
+var geometry, material, mesh;
+var isAnimated = false;
+var isRight = true;
+var k = 0.05;
+var request;
+
+function init() {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 500;
+    scene = new THREE.Scene();
+    geometry = new THREE.CubeGeometry(250, 250, 250);
+    material = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true});
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(1066, 466);
+    renderer.setClearColorHex(0xffffff, 0.3);
+    document.getElementsByClassName("canvas")[0].appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+}
