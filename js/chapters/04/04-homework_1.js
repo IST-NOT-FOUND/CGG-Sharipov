ad18a94ef7e2f96802181ea2b04f70907c4aa5d8
qusijue <qusijue@gmail.com>
2017-12-12 03:42:17 +0500

    chore (js\chapters\04\04-homework_1.js) add function getShader

--- js/chapters/04/04-homework_1.js
+++ js/chapters/04/04-homework_1.js
@@ -29,3 +29,16 @@ function setMatrixUniforms(){
     gl.uniformMatrix4fv(shaderProgram.ProjMatrix,false, pMatrix);
     gl.uniformMatrix4fv(shaderProgram.MVMatrix,false, mvMatrix);
 }
+
+function getShader(type,id) {
+    var source = document.getElementById(id).innerHTML;
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Shader compilation error: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
