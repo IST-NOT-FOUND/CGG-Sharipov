7cf33918137b0eba218699a5709ddc0f519631c9
qusijue <qusijue@gmail.com>
2017-12-12 04:16:43 +0500

    chore (js\chapters\05\02-rotating-cube.js) add function window.requestAnimFrame

--- js/chapters/05/02-rotating-cube.js
+++ js/chapters/05/02-rotating-cube.js
@@ -105,3 +105,14 @@ window.onload = function(){
         })();
     }
 };
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback, element) {
+            return window.setTimeout(callback, 1000/60);
+        };
+})();
