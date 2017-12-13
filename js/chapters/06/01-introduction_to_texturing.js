bf78f584ba85350035a0d3960df5c90cd00a0350
qusijue <qusijue@gmail.com>
2017-12-13 05:08:34 +0500

    chore (js\chapters\06\01-introduction_to_texturing.js) add function initBuffers

--- js/chapters/06/01-introduction_to_texturing.js
+++ js/chapters/06/01-introduction_to_texturing.js
@@ -31,3 +31,21 @@ function getShader(type,id) {
     }
     return shader;
 }
+
+function initBuffers() {
+    var vertices =[
+        -0.5, -0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5
+    ];
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = 3;
+    var indices = [0, 1, 2, 2, 3, 0];
+    indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    indexBuffer.numberOfItems = indices.length;
+}
