91a1a03edb5f9dfd7e5d5ea95627248b8da99c3d
qusijue <qusijue@gmail.com>
2017-12-13 07:06:30 +0500

    chore (js\chapters\07\01-fong_reflection.js) add function setTextures

--- js/chapters/07/01-fong_reflection.js
+++ js/chapters/07/01-fong_reflection.js
@@ -160,3 +160,15 @@ function setupWebGL() {
     mat4.rotateY(mvMatrix,mvMatrix, yAngle);
     mat3.normalFromMat4(nMatrix, mvMatrix);
 }
+
+function setTextures(){
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    var image = new Image();
+    image.onload = function() {
+        handleTextureLoaded(image, texture);
+    };
+    image.src = "../../src/images/texture/lava.png";
+    shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
+    gl.uniform1i(shaderProgram.samplerUniform, 0);
+}
