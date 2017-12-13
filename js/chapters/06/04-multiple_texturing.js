1decfc5bb9ecfc054a559eef1b4faad03e1ea8c7
qusijue <qusijue@gmail.com>
2017-12-13 05:24:55 +0500

    chore (js\chapters\06\04-multiple_texturing.js) add function setupWebGL

--- js/chapters/06/04-multiple_texturing.js
+++ js/chapters/06/04-multiple_texturing.js
@@ -207,3 +207,13 @@ function roofDraw() {
     gl.enable(gl.DEPTH_TEST);
     gl.drawElements(gl.TRIANGLES, roofIndexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
 }
+
+function setupWebGL() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    mat4.perspective(pMatrix, 1.04, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
+    mat4.identity(mvMatrix);
+    mat4.translate(mvMatrix,mvMatrix,[0, 0, zTranslation]);
+    mat4.rotate(mvMatrix,mvMatrix, angle, [0, 1, 0]);
+}
