5bc5e14c8eec9f2b380579f9992cf52e072093b0
qusijue <qusijue@gmail.com>
2017-12-13 07:06:58 +0500

    chore (js\chapters\07\01-fong_reflection.js) add function handleTextureLoaded

--- js/chapters/07/01-fong_reflection.js
+++ js/chapters/07/01-fong_reflection.js
@@ -172,3 +172,12 @@ function setTextures(){
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 }
+
+function handleTextureLoaded(image, texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
