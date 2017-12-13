10d9d5820c18a55c1bb5d58553e3980fdaff589c
qusijue <qusijue@gmail.com>
2017-12-13 07:04:14 +0500

    chore (js\chapters\07\01-fong_reflection.js) add function setLights

--- js/chapters/07/01-fong_reflection.js
+++ js/chapters/07/01-fong_reflection.js
@@ -37,3 +37,10 @@ function initShaders() {
     shaderProgram.uniformDiffuseLightColor = gl.getUniformLocation(shaderProgram, "uDiffuseLightColor");
     shaderProgram.uniformSpecularLightColor = gl.getUniformLocation(shaderProgram, "uSpecularLightColor");
 }
+
+function setupLights() {
+    gl.uniform3fv(shaderProgram.uniformLightPosition, [0.0, 10.0, 5.0]);
+    gl.uniform3fv(shaderProgram.uniformAmbientLightColor, [0.1, 0.1, 0.1]);
+    gl.uniform3fv(shaderProgram.uniformDiffuseLightColor, [0.7, 0.7, 0.7]);
+    gl.uniform3fv(shaderProgram.uniformSpecularLightColor, [1.0, 1.0, 1.0]);
+}
