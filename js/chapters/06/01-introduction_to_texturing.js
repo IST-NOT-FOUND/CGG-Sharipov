7071ad75c905b1b8c19d5d42544a7ba30e583775
qusijue <qusijue@gmail.com>
2017-12-13 05:10:11 +0500

    chore (js\chapters\06\01-introduction_to_texturing.js) add function handleTextureLoaded

--- js/chapters/06/01-introduction_to_texturing.js
+++ js/chapters/06/01-introduction_to_texturing.js
@@ -76,3 +76,11 @@ function setTextures(){
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 }
+
+function handleTextureLoaded(image, texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+}
