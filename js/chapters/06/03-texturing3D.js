559334ce51c645b509429d612ce9b669f31c486f
qusijue <qusijue@gmail.com>
2017-12-13 05:20:32 +0500

    chore (js\chapters\06\03-texturing3D.js) add function window.requestAnimFrame

--- js/chapters/06/03-texturing3D.js
+++ js/chapters/06/03-texturing3D.js
@@ -191,3 +191,14 @@ function handleKeyDown(e){
             break;
     }
 }
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback, element) {
+            return window.setTimeout(callback, 1000/60);
+        };
+})();
