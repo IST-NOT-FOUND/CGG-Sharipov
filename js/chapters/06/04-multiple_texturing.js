9dba4fcc71fcbfaa55e102fe5314a70bc2e2db81
qusijue <qusijue@gmail.com>
2017-12-13 05:28:01 +0500

    chore (js\chapters\06\04-multiple_texturing.js) add function window.requestAnimFrame

--- js/chapters/06/04-multiple_texturing.js
+++ js/chapters/06/04-multiple_texturing.js
@@ -286,3 +286,15 @@ function handleKeyDown(e){
             break;
     }
 }
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback, element) {
+            return window.setTimeout(callback, 1000/60);
+        };
+
+})();
