2292d21c5b84e5f1030059bf14e86163d524bf36
qusijue <qusijue@gmail.com>
2017-12-13 05:21:34 +0500

    chore (js\chapters\06\04-multiple_texturing.js) add function initShaders

---
+++ js/chapters/06/04-multiple_texturing.js
@@ -0,0 +1,33 @@
+var gl;
+var shaderProgram;
+var wallVertexBuffer;
+var wallIndexBuffer;
+var roofVertexBuffer;
+var roofIndexBuffer;
+var wallTextureCoordsBuffer;
+var roofTextureCoordsBuffer;
+var wallTexture;
+var roofTexture;
+var angle = 2.0;
+var zTranslation = -2.0;
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+function initShaders() {
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Failed to install shaders");
+    }
+    gl.useProgram(shaderProgram);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexTextureAttribute = gl.getAttribLocation(shaderProgram, "aVertexTextureCoords");
+    gl.enableVertexAttribArray(shaderProgram.vertexTextureAttribute);
+    shaderProgram.MVMatrix = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.ProjMatrix = gl.getUniformLocation(shaderProgram, "uPMatrix");
+}
