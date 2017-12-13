7813f92472c72fbbf80bed325c564f7158ef3f08
qusijue <qusijue@gmail.com>
2017-12-13 05:12:00 +0500

    chore (js\chapters\06\02-texture_coordinates.js) add function initShaders

---
+++ js/chapters/06/02-texture_coordinates.js
@@ -0,0 +1,23 @@
+var gl;
+var shaderProgram;
+var vertexBuffer;
+var indexBuffer;
+var textureCoordsBuffer;
+var texture;
+
+function initShaders() {
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Failed to install shaders");
+    }
+    gl.useProgram(shaderProgram);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexTextureAttribute = gl.getAttribLocation(shaderProgram, "aVertexTextureCoords");
+    gl.enableVertexAttribArray(shaderProgram.vertexTextureAttribute);
+}
