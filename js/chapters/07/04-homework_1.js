8fdd527155b671b5c1208da0f69976fb8d7231c0
qusijue <qusijue@gmail.com>
2017-12-16 05:44:06 +0500

    chore (js\chapters\07\04-homework_1.js) add function loadShader

--- js/chapters/07/04-homework_1.js
+++ js/chapters/07/04-homework_1.js
@@ -366,3 +366,14 @@ function initShaderProgram(gl, vsSource, fsSource) {
     return shaderProgram;
 }
 
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
